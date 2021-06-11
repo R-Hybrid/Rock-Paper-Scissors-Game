33c67b31b21de9a807c0cc7954c7e700e28d84db
Ryan M <31717006+R-Hybrid@users.noreply.github.com>
2021-06-11 23:12:25 +0000

    Finished code with video help and debugged.

--- main.py
+++ main.py
@@ -27,5 +27,33 @@ scissors = '''
 ---.__(___)
 '''
 
-print("Welcome to Rock, Paper, Scissors")
-player_choice = input("")
+images = [rock, paper, scissors]
+
+print("Welcome to Rock, Paper, Scissors!")
+player_choice = int(input("Please pick one. 0 for Rock, 1 for Paper, or 2 for Scissors."))
+if player_choice >= 3 or player_choice < 0:
+  print("You typed an invalid number.")
+else:
+  #Images print first, then logic is run below
+  print(images[player_choice])
+  computer_choice = random.randint(0, 2)
+  print(f"The computer chose:")
+  print(images[computer_choice])
+
+
+  #player picks rock (0), computer picks scissors (2)
+  if player_choice == 0 and computer_choice == 2:
+    print("You win!") 
+  #player picks scissors (2), computer picks rock (0)
+  elif computer_choice == 0 and player_choice == 2:
+    print("You lose.")
+  #player picks rock or paper but computer picks paper or scissors 
+  elif computer_choice > player_choice:
+    print("You lose.")
+  #computer picks rock or paper but player picks paper or scissors
+  elif player_choice > computer_choice:
+    print("You win!")
+  #both pick the same number  
+  elif computer_choice == player_choice:
+    print("It's a draw!")
+
